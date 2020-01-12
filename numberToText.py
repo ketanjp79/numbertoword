74ee72070fe932e58a8926f801fcb6e44ab7394e
ketan patel <ketanjp79@yahoo.com>
2020-01-12 21:21:12 +0400

    updated author name and shebang

--- numberToText.py
+++ numberToText.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+#Author:ketan patel
+
 num = """1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 30 40 50 60 70 80 90 """.split(" ")
 
 numText="""One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety """.split(" ")
